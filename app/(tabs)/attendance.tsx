ba594cdd0bca0630784277908d8401f05283471c
Ticarat Andrei <ticarat.andrei125@gmail.com>
2026-04-16 15:03:34 +0300

    chore: add AttendanceScreen component to tabs structure

---
+++ app/(tabs)/attendance.tsx
@@ -0,0 +1,13 @@
+// app/(tabs)/attendance.tsx
+
+
+
+import {View, Text} from "react-native";
+
+export default function AttendanceScreen() {
+    return (
+        <View>
+            <Text>Attendance Screen</Text>
+        </View>
+    )
+}
