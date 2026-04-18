ffbddecd417bdc172a998094656d19bebd0c06aa
Ticarat Andrei <ticarat.andrei125@gmail.com>
2026-04-18 19:51:34 +0300

    chore: add request interceptor to include auth token in API requests

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,7 @@
 // src/services/api.ts
 
 import axios from 'axios';
+import {getItem} from '@/src/services/storage';
 
 const API_URL = 'http://127.0.0.1:8000/';
 
@@ -12,3 +13,25 @@ export const api = axios.create(
         },
     }
 )
+
+// Interceptor for adding the token to the request headers
+api.interceptors.request.use(
+    async (config) => {
+
+        try {
+            const token = await getItem('@stafy_token')
+
+            if (token) {
+                config.headers.Authorization = `Bearer ${token}`
+            }
+
+            return config;
+        } catch (error) {
+            console.error("Error getting token from storage:", error);
+            return config;
+        }
+
+
+    }, (error) => {
+        return Promise.reject(error);
+    })
