e32de2d9c5fef475c51b4d664f0ac4b24fc60490
Andrei Ticarat <ticarat.andrei125@gmail.com>
2026-04-25 20:52:59 +0300

    chore: integrate routeHelper in HeaderThemed for dynamic page titles

--- src/components/HeaderThemed.tsx
+++ src/components/HeaderThemed.tsx
@@ -3,21 +3,26 @@
 import {View, Text} from "react-native";
 import {useState} from "react";
 import useUser from "@/src/hooks/useUser";
+import {usePathname} from "expo-router";
+import routeHelper from "@/src/utils/routeHelper";
 
 
 export default function HeaderThemed() {
 
+    const pathname = usePathname();
+
     const {user} = useUser();
 
-    const [name, setName] = useState("");
+    // get the page title
+    const pageTitle: string = routeHelper(pathname)
 
     return(
         <View>
             {/* Future: Image Icon */}
 
-            <View>
-                <Text>Bună, {user?.first_name || "Utilizator"}!</Text>
-                <Text>Adaugare Pontaj!</Text>
+            <View className={"bg-white shadow-sm flex-column items-start pl-5 py-2"}>
+                <Text className={"text-secondary-500 font-semibold"}>Bună, {user?.first_name || "Utilizator"}!</Text>
+                <Text className={"text-lg font-bold"}>{pageTitle}</Text>
             </View>
 
         </View>
